85abfe20ed6b1e512a5417261f188762ebcfcdb7
PhuPing <sakurajmira.shion@gmail.com>
2018-10-12 10:42:41 +0700

    sadfjk;l

--- Sample.py
+++ Sample.py
@@ -1,6 +1,7 @@
 print("I don't like sand. It's course, and rough, and irritating... and it gets everywhere.")
 print("'_'")
 print("It's over Anakin, I have the high ground.")
+print("It's over Anakin, I have the high ground.")
 
 print("Hello (/owo)/")
 #asdasd
