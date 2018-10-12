8a3ff64d98265128570e97961bd81c2aab634f64
The taller Nut <nutsisky@gmail.com>
2018-10-12 10:28:11 +0700

    no change

--- Sample.py
+++ Sample.py
@@ -1,5 +1,3 @@
 print("I don't like sand. It's course, and rough, and irritating... and it gets everywhere.")
 print("'_'")
 print("It's over Anakin, I have the high ground.")
-
-print("Hello (/owo)/")
