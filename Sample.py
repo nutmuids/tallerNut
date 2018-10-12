ac501f5ae021b5c4964cb9f02aa22db19c4a3e05
PhuPing <sakurajmira.shion@gmail.com>
2018-10-12 10:49:15 +0700

    asdfjklads

--- Sample.py
+++ Sample.py
@@ -1,8 +1,8 @@
 print("I don't like sand. It's course, and rough, and irritating... and it gets everywhere.")
 print("'_'")
 print("It's over Anakin, I have the high ground.")
-
-
+print("It's over Anakin, I have the high ground.")
+print("You, underestimate my POWAH!!")
 print("Hello (/owo)/")
 #asdasd
 #asdjfkasdfj;asdfk
